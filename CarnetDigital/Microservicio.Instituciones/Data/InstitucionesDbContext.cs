fa0d5405325e5ed105e2c9a45805fa42c5c37564
Sergio Monge <serale1804@gmail.com>
2026-06-10 00:51:20 -0600

    feat: Microservicio.Instituciones (SRV2) implementado con autenticacion JWT y bitacora

---
+++ CarnetDigital/Microservicio.Instituciones/Data/InstitucionesDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microservicio.Instituciones.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservicio.Instituciones.Data
+{
+    public class InstitucionesDbContext : DbContext
+    {
+        public InstitucionesDbContext(DbContextOptions<InstitucionesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Institucion> Instituciones { get; set; }
+        public DbSet<InstitucionDominio> InstitucionDominios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuración: Institución → Dominios (1 a muchos)
+            modelBuilder.Entity<InstitucionDominio>()
+                .HasOne(d => d.Institucion)
+                .WithMany(i => i.Dominios)
+                .HasForeignKey(d => d.IdInstitucion)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Nombres de tabla explícitos
+            modelBuilder.Entity<Institucion>().ToTable("Instituciones");
+            modelBuilder.Entity<InstitucionDominio>().ToTable("InstitucionDominios");
+        }
+    }
+}
---
+++ CarnetDigital/Microservicio.Instituciones/Interfaces/IBitacoraService.cs
@@ -0,0 +1,7 @@
+﻿namespace Microservicio.Instituciones.Interfaces
+{
+    public interface IBitacoraService
+    {
+        Task RegistrarAsync(int usuarioId, string descripcion, string token);
+    }
+}
